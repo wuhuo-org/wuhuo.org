d585bacb973687bedf65b4b59a338bbdd92e1999
Huo Yun <cloudblaze@yeah.net>
2017-08-26 17:50:06 +0800

    修改登录页面为Ajax方式。

--- src/login.php
+++ src/login.php
@@ -1,34 +1,66 @@
 <?php
-	session_start();
-	unset($_SESSION['linux011-user']);
-	
-	$username = $_GET['username'];
-	$passwd = $_GET['passwd'];
-	
-	if ($username && $passwd) {
-		$db = new mysqli('127.0.0.1', 'linux011', 'ikm-098', 'linux011');
-		$db->query("set character set 'utf8'");//读库
 
-		$sql = "SELECT id, zhang_hao FROM zhang_hao WHERE zhang_hao='".$username."' AND mi_ma='".$passwd."'";
-		$result = $db->query($sql);
+/*
+ * function: 用户登录。
+ * argument:
+ * 	@username
+ * 		用户名
+ * 	@password
+ * 		密码 
+ * return value:
+ * 	format: {"result": "成功标识，0为成功；1为失败", "message": "描述信息。"}
+ * 	@success: {"result": "0", "message": "登录成功！"}
+ * 	@failure: {"result": "1", "message": "登录失败！"}
+ */
 
-  		if($result->num_rows>0){
-  			$row=$result->fetch_assoc();
- 			$str = $row['zhang_hao'] . ":)" . $row['id'];
-   			$_SESSION['linux011-user'] = $str;
-			require_once("index.php");
-			$result->free;
-			$db->close;	
-  		}
-		else{
-			require_once('login.php');			
-			echo '<br/><hr/><p align="center" style="color:red">账号或密码错误！</p>';
- 			$result->free;
-			$db->close;
-  		}
-	}
-	else{
-		require_once('login.php');			
-		echo '<br/><hr/><p align="center" style="color:red">账号或密码错误！</p>';
-	}
+$username = $_POST['username'];
+$password = $_POST['password'];
+$response = "";
+
+//echo '{"result": "1", "message": "username = ' . $username . ', password = ' . $password . '"}';
+//return;
+
+if(!($username && $password))
+{
+	$response = '{"result": "1", "message": "用户名、密码不能为空！"}';
+	goto L_return;
+}
+
+include("config.php");
+$conn = new mysqli($db_host, $db_username, $db_password, $db_name);
+if(!$conn)
+{
+	$response = '{"result": "1", "message": "服务器发生错误！"}';
+	goto L_return;
+}
+
+mysqli_set_charset($conn,"utf8");
+
+$sql = "SELECT id, zhang_hao FROM zhang_hao WHERE zhang_hao='" . $username . "' AND mi_ma='" . $password . "'";
+$result = $conn->query($sql);
+if($result->num_rows == 0)
+{
+	$response = '{"result": "1", "message": "帐号或密码错误！"}';
+	goto L_close_db;
+}
+else if($result->num_rows > 1)
+{
+	$response = '{"result": "1", "message": "服务器发生错误！"}';
+	goto L_close_db;
+}
+
+session_start();
+$row=$result->fetch_assoc();
+$str = $row['zhang_hao'] . ":)" . $row['id'];
+$_SESSION['linux011-user'] = $str;
+$response = '{"result": "0", "message": "登录成功！"}';
+
+L_close_db:
+$result->free;
+$conn->close;
+goto L_return;
+
+L_return:
+echo $response;
+
 ?>
--- src/register.php
+++ src/register.php
@@ -11,7 +11,7 @@
  * 		邮箱
  * return value:
  * 	format: {"result": "成功标识，0为成功；1为失败", "message": "描述信息。"}
- * 	@success: {"result": "0", "message": "注册用户成功。"}
+ * 	@success: {"result": "0", "message": "注册用户成功！"}
  * 	@failure: {"result": "1", "message": "服务器发生错误。"}
  */
 
@@ -20,41 +20,47 @@ include("config.php");
 $username = $_POST['username'];
 $password = $_POST['password'];
 $email = $_POST['email'];
+$response = "";
 
-if($username && $password && $email)
+if(!($username && $password && $email))
 {
-	$conn = new mysqli($db_host, $db_username, $db_password, $db_name);
-	if(!$conn)
-	{
-		echo '{"result": "1", "message": "服务器发生错误！"}';
-		return;
-	}
-
-	mysqli_set_charset($conn,"utf8");
-
-	$sql = "SELECT * FROM zhang_hao WHERE zhang_hao = '" . $username. "'";
-	$result = $conn->query($sql);
-	if($result->num_rows){
-		echo '{"result": "1", "message": "用户名已存在！"}';
-		return;
-	}
-	$result->free;
-
-	$sql = "INSERT INTO zhang_hao SET zhang_hao='" . $username. "', mi_ma='" . $password . "', you_xiang ='" . $email . "'";
-	$result = $conn->query($sql);
-  	if($conn->affected_rows != 1){
-		echo '{"result": "1", "message": "注册失败！"}';
-		return;
-	}
-
-	$result->free;
-	$conn->close;
-	echo '{"result": "0", "message": "注册用户成功。"}';
+	$response = '{"result": "1", "message": "用户名、密码及邮箱不能为空！"}';
+	goto L_return;
 }
-else
+
+$conn = new mysqli($db_host, $db_username, $db_password, $db_name);
+if(!$conn)
 {
-	echo '{"result": "1", "message": "用户名、密码及邮箱不能为空！"}';
-	return;
+	$response = '{"result": "1", "message": "服务器发生错误！"}';
+	goto L_return;
 }
+
+mysqli_set_charset($conn,"utf8");
+	
+$sql = "SELECT * FROM zhang_hao WHERE zhang_hao = '" . $username. "'";
+$result = $conn->query($sql);
+if($result->num_rows != 0)
+{
+	$response = '{"result": "1", "message": "用户名已存在！"}';
+	goto L_close_db;
+}
+$result->free;
+
+$sql = "INSERT INTO zhang_hao SET zhang_hao='" . $username. "', mi_ma='" . $password . "', you_xiang ='" . $email . "'";
+$result = $conn->query($sql);
+if($conn->affected_rows != 1)
+{
+	$response = '{"result": "1", "message": "注册失败！"}';
+	goto L_close_db;
+}
+$response = '{"result": "0", "message": "注册用户成功！"}';
+
+L_close_db:
+$result->free;
+$conn->close;
+goto L_return;
+
+L_return:
+echo $response;
 
 ?>
